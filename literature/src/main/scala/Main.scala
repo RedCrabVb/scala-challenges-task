947a035972fe01de06f9503db08ad3329a06a354
RedCrabVb <cany245@mail.ru>
2022-01-17 09:51:33 +0300

    Main scala 3

--- literature/src/main/scala/Main.scala
+++ literature/src/main/scala/Main.scala
@@ -15,7 +15,7 @@ import cats.effect.unsafe.implicits.global
 
 //https://raw.githubusercontent.com/benschw/shakespeare-txt/master/shakespeare-hamlet-25.txt
 object Main {
-  def main(args: Array[String]): Unit = {
+  @main def main(wegPage: String): Unit = {
     val cpuPool: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
 
     def countWorld(words: String) = IO {
@@ -27,7 +27,7 @@ object Main {
     }
 
     val result = (for {
-      text <- IO { Source.fromURL(args(0)).getLines().mkString(" ") }
+      text <- IO { Source.fromURL(wegPage).getLines().mkString(" ") }
       words <- countWorld(text).evalOn(cpuPool)
       chars <- countChar(text).evalOn(cpuPool)
     } yield {
