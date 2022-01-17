72e5f5548f470526fe7c8c4fd07bf4dadcc8f7d1
RedCrabVb <cany245@mail.ru>
2022-01-17 09:29:59 +0300

    Add old change

--- literature/build.sbt
+++ literature/build.sbt
@@ -3,3 +3,7 @@ name := "literature"
 version := "0.1"
 
 scalaVersion := "3.1.0"
+
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-effect" % "3.3.0"
+)
--- literature/src/main/scala/Main.scala
+++ literature/src/main/scala/Main.scala
@@ -1,31 +1,41 @@
+import cats.effect.IO
+
 import java.time.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 import java.util.concurrent.Executors
 import scala.io.{BufferedSource, Source}
 import scala.sys.exit
 import scala.util.{Failure, Success}
+
+import cats.effect.IO
+
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext
+import cats.effect.unsafe.implicits.global
+
 //https://raw.githubusercontent.com/benschw/shakespeare-txt/master/shakespeare-hamlet-25.txt
-
 object Main {
-  given ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
-
-  def avgStrInText(text: String): Float = {
-    val lengthStr = text.split(" ").map(_.length)
-
-    (lengthStr.sum.toFloat / lengthStr.length)
-  }
-
-
   def main(args: Array[String]): Unit = {
-    val text = Future[String] {
-      Source.fromURL(args(0)).getLines().mkString(" ")
+    val cpuPool: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
+
+    def countWorld(words: String) = IO {
+      words.split(" ").length
     }
-    text.onComplete {
-      case Success(text) => {
-        printf("%.1f\n", avgStrInText(text))
-        exit(0)
-      }
-      case Failure(t) => t.printStackTrace()
+
+    def countChar(line: String) = IO {
+      line.replace(" ", "").length
     }
+
+    val result = (for {
+      text <- IO { Source.fromURL(args(0)).getLines().mkString(" ") }
+      words <- countWorld(text).evalOn(cpuPool)
+      chars <- countChar(text).evalOn(cpuPool)
+    } yield {
+      chars.toFloat / words.toFloat
+    }).unsafeRunSync()
+
+    printf("%.1f\n", result)
+    exit(0)
   }
+
 }
