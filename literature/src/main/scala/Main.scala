ff144994abc3509b77d923397c0ab45bbd6c0ec7
RedCrabVb <cany245@mail.ru>
2022-01-20 16:19:06 +0300

    Fix error

--- literature/src/main/scala/Main.scala
+++ literature/src/main/scala/Main.scala
@@ -14,28 +14,27 @@ import scala.concurrent.ExecutionContext
 import cats.effect.unsafe.implicits.global
 
 //https://raw.githubusercontent.com/benschw/shakespeare-txt/master/shakespeare-hamlet-25.txt
-object Main {
-  @main def main(wegPage: String): Unit = {
-    val cpuPool: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
+@main def main(wegPage: String): Unit = {
+  val threadPull = Executors.newFixedThreadPool(2)
+  val cpuPool: ExecutionContext = ExecutionContext.fromExecutor(threadPull)
 
-    def countWorld(words: String) = IO {
-      words.split(" ").length
-    }
-
-    def countChar(line: String) = IO {
-      line.replace(" ", "").length
-    }
-
-    val result = (for {
-      text <- IO { Source.fromURL(wegPage).getLines().mkString(" ") }
-      words <- countWorld(text).evalOn(cpuPool)
-      chars <- countChar(text).evalOn(cpuPool)
-    } yield {
-      chars.toFloat / words.toFloat
-    }).unsafeRunSync()
-
-    printf("%.1f\n", result)
-    exit(0)
+  def countWorld(words: String) = IO {
+    words.split(" ").length
   }
 
+  def countChar(line: String) = IO {
+    line.replace(" ", "").length
+  }
+
+  val result = (for {
+    text <- IO { Source.fromURL(wegPage).getLines().mkString(" ") }
+    words <- countWorld(text).evalOn(cpuPool)
+    chars <- countChar(text).evalOn(cpuPool)
+  } yield {
+    chars.toFloat / words.toFloat
+  }).unsafeRunSync()
+  threadPull.shutdown()
+
+  printf("%.1f\n", result)
 }
+
