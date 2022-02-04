bf63bb791fd57d65002496b0909277b6fb1e9f6f
RedCrabVb <cany245@mail.ru>
2022-02-04 16:43:46 +0300

    Fix code

--- wc/src/main/scala/MainFS2.scala
+++ wc/src/main/scala/MainFS2.scala
@@ -13,22 +13,25 @@ def Main(name: String) = {
       new FileInputStream(name)
     }, chunkSize = 4096, closeAfterUse = true)
 
-  val lineSize = file
+  val lineSize: Stream[IO, Int] = file
     .through(text.utf8Decode)
     .through(text.lines)
     .map(_.length)
 
-  val countLine: Stream[IO, Int] = lineSize.fold(0)((prev, next) => prev + 1)
+  case class State(countLine: Int, maxLine: Int, countChar: Int) {
+    def combine(addState: State): State = {
+      State(countLine + addState.countLine, Math.max(maxLine, addState.maxLine), countChar + addState.countChar)
+    }
+  }
 
-  val maxLine: Stream[IO, Int] = lineSize
-    .fold(0)((prev, next) => Math.max(next, prev))
 
-  val charCount: Int => Stream[IO, Int] = (first: Int) => lineSize
-    .fold(first)((prev, next) => prev + next)
+  val resultStateIO: IO[State] = lineSize
+    .fold(State(-1, 0, -1))((state, l) => state.combine(State(1, l, l + 1)))
+    .compile.toList.map(_.head)
 
-  val result = countLine.onComplete(maxLine).onComplete(charCount(0)).compile.toList.unsafeRunSync()
+  val resultState = resultStateIO.unsafeRunSync()
 
-  println("CountLine: " + (result(0).toInt - 1))
-  println("MaxLine: " + result(1))
-  println("CountChar: " + (result(2).toInt + result(0).toInt - 1))
+  println("CountLine: " + resultState.countLine)
+  println("MaxLine: " + resultState.maxLine)
+  println("CountChar: " + resultState.countChar)
 }
